b2cda0aaf2e48cc5818247991bc5231073610577
Eugene Kosukhin <kosuha606@gmail.com>
2026-04-28 23:35:08 +0300

    build: before release

---
+++ dist/silentium-loop.d.ts
@@ -0,0 +1,25 @@
+type CommandNextType = (state: any, ...rest: any[]) => any;
+type CommandFailType = (state: any, reason: any) => any;
+type CommandType = {
+    type: string;
+    args?: any[];
+    next?: CommandNextType;
+    fail?: CommandNextType;
+};
+type CommandsType = (CommandType | CommandType[])[];
+type StateWithCommands = {
+    commands: CommandsType;
+};
+type CommandSetter<T> = (state: T, command: CommandType) => T;
+declare function createCommand<T>(commandPush?: CommandSetter<T>): {
+    Command(state: T, command: CommandType): T;
+    BatchCommand(state: T, commands: CommandType[]): void;
+};
+
+type DispatchType = (fn: (...args: any[]) => any) => any;
+type StoreActionType = (command: CommandType, dispatch: DispatchType) => Promise<any>;
+type StoreActionProvider = [string, StoreActionType];
+declare function Action(dispatch: DispatchType, actionsConfig: StoreActionProvider[], getCommands: () => CommandType[], resetCommands?: (state: any) => unknown, warning?: (...data: any[]) => void): () => Promise<void>;
+
+export { Action, createCommand };
+export type { CommandFailType, CommandNextType, CommandSetter, CommandType, CommandsType, DispatchType, StateWithCommands, StoreActionProvider, StoreActionType };
---
+++ dist/silentium-loop.js
@@ -0,0 +1,81 @@
+const defaultResetCommands = (state) => {
+  state.commands = [];
+};
+function Action(dispatch, actionsConfig, getCommands, resetCommands = defaultResetCommands, warning = console.warn) {
+  const actions = actionsConfig.map(function ActionHandlerMap([type, action]) {
+    return { type, action };
+  });
+  const handlersGroups = groupBy(actions, "type");
+  return async function ActionListener() {
+    const commands = getCommands();
+    if (commands === void 0 || !Array.isArray(commands)) {
+      throw new Error(
+        "WARNING! commands are undefined, possibly problem in you logic!"
+      );
+    }
+    if (commands.length === 0) {
+      return;
+    }
+    dispatch(resetCommands);
+    const stateCommands = commands ? [...commands] : [];
+    const unhandledCommandTypes = [];
+    while (stateCommands.length > 0) {
+      let commands2 = stateCommands.shift() ?? [];
+      if (!Array.isArray(commands2)) {
+        commands2 = [commands2];
+      }
+      await Promise.all(
+        commands2.map(async function ActionCommandsMap(command) {
+          const handlersForType = handlersGroups[command.type];
+          if (handlersForType?.length) {
+            for (const handler of handlersForType) {
+              await handler.action(command, dispatch);
+            }
+          } else {
+            unhandledCommandTypes.push(command.type);
+          }
+        })
+      );
+    }
+    if (unhandledCommandTypes.length) {
+      const unhandledTypes = [...new Set(unhandledCommandTypes)];
+      warning("Unhandled commands in store!", unhandledTypes);
+    }
+  };
+}
+function groupBy(list, key) {
+  return list.reduce(
+    function GroupByReduce(previous, currentItem) {
+      const keyValue = String(currentItem[key]);
+      if (keyValue === void 0) {
+        throw new Error("Group by no such key " + String(key));
+      }
+      if (!previous[keyValue]) {
+        previous[keyValue] = [];
+      }
+      previous[keyValue].push(currentItem);
+      return previous;
+    },
+    {}
+  );
+}
+
+const defaultCommandPush = (state, command) => {
+  state.commands.push(command);
+};
+function createCommand(commandPush = defaultCommandPush) {
+  const buildedCommands = {
+    Command(state, command) {
+      return commandPush(state, command);
+    },
+    BatchCommand(state, commands) {
+      commands.reduce(function BatchReduce(last, command) {
+        return commandPush(last, command);
+      }, state);
+    }
+  };
+  return buildedCommands;
+}
+
+export { Action, createCommand };
+//# sourceMappingURL=silentium-loop.js.map
