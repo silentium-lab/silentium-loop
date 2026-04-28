5dd0d615d9877d9834df5137d2aab0e479a127e4
Eugene Kosukhin <kosuha606@gmail.com>
2026-04-28 23:34:49 +0300

    fix: default command push

--- src/Command.ts
+++ src/Command.ts
@@ -17,30 +17,20 @@ export type StateWithCommands = {
 
 export type CommandSetter<T> = (state: T, command: CommandType) => T;
 
-const defaultSetter: CommandSetter<any> = (state, command) => {
+const defaultCommandPush: CommandSetter<any> = (state, command) => {
   state.commands.push(command);
 };
 
 export function createCommand<T>(
-  commandSetter: CommandSetter<T> = defaultSetter,
-  returnState = true,
+  commandPush: CommandSetter<T> = defaultCommandPush,
 ) {
   const buildedCommands = {
     Command(state: T, command: CommandType) {
-      if (returnState) {
-        return commandSetter(state, command);
-      }
-      commandSetter(state, command);
+      return commandPush(state, command);
     },
     BatchCommand(state: T, commands: CommandType[]) {
-      if (returnState) {
-        return commands.reduce(function BatchReduce(last, command) {
-          return commandSetter(last, command);
-        }, state);
-      }
       commands.reduce(function BatchReduce(last, command) {
-        commandSetter(last, command);
-        return last;
+        return commandPush(last, command);
       }, state);
     },
   };
